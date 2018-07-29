b1f04a61f304b84daed5f6405dc207ff7fe46514
Rachel Jacobs <rachelsarajacobs@gmail.com>
2018-07-29 15:04:31 -0500

    Add Javascript File

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,115 @@
+// document.write("Hello World!");
+// console.log("Hi, this is Rachel!");
+
+// var firstName = "Rachel";
+// console.log(firstName);
+
+// var greeting = "Hello " + "world";
+// // console.log(greeting);
+// // console.log(greeting.length);
+// console.log(greeting.toLowerCase());
+// console.log(greeting.includes("e"));
+// console.log(greeting.indexOf("e"));
+// console.log(greeting.substring(1, 4));
+
+// var name = prompt("Tell me your name!");
+// document.write("<h1>Hello, " + name + ", welcome!</h1>");
+
+// var number = 4; 
+// console.log(number + 2);
+
+// function sayHello(name) {
+// 	console.log("Hello, " + name);
+
+// }
+// sayHello("Rachel");
+
+// function add() {
+
+// 	var num1 = parseInt(prompt("Please give me a number!"));
+// 	var num2 = parseInt(prompt ("Now, another number!"));
+// 	var sum = num1 + num2;
+// 	console.log(sum);
+
+// }
+// function addTwoNumbers (num1,num2) {
+// 	return num1 + num2
+// }
+
+// var firstName = prompt("Tell me your first name!");
+// var lastName = prompt("Tell me your last name!");
+// console.log("firstName" + " " + "lastName");
+
+
+// function fullName(firstName,lastName) {
+// 	var fullName = firstName + " " + lastName;
+// 	console.log(fullName);
+// // }
+// // fullName("Rachel","Jacobs")
+
+// function increment(number) {
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// }
+// increment (5);
+
+// if (4 === 4) {
+// 	console.log("This is true!");
+// // }
+
+// function getNumber () {
+// 	var number = parseInt(prompt("Please enter a 2!"));
+// 	if (number === 2) {
+// 		console.log("Thanks for the 2!");
+// 	} else if (number === 1) {
+// 		console.log("That's a 1.");
+
+// 	} else {
+// 		console.log("That's not a 2...");
+
+// 	}
+// }
+
+// getNumber();
+
+
+// function joke () {
+// 	var greeting = prompt("Tell me a joke!");
+
+// 	if (greeting === "Knock Knock" || greeting === "knock knock")}
+// 		console.log("Who's there?");
+
+// }
+
+// function logNumbers(number) {
+// 	var counter = 0
+
+// 	while (counter < number) {
+// 		counter += 1;
+// 		console.log(counter);
+// 	};
+// }
+// for (var counter = 0; counter <10; counter += 1) {console.log(i)
+// }
+
+function increment(number) {
+	for (var i = 1; i <= number; i += 1) {
+		conole.log(i);
+	}
+increment(10);
+
+function letters(string) {
+	for (var i = 0; i <= string.length )
+}
+
+	}
+}
+}
